5b42df2e651f10843e218eccc5654e6daa0d8049
IGOR JOSUE SILVA MORAIS <igor.jsmorais@senacsp.edu.br>
2023-08-25 08:23:22 -0300

    Apenas Testes

---
+++ teste1.py
@@ -0,0 +1,13 @@
+cliente = ['carlos', ['(11) 09876-543', 'carlos@gmail.com'], [4.5, 7, 9]]
+
+print(cliente[0])#pegando o nome do cliente
+
+print(cliente[1][0])#pegando o numero do cliente
+
+print(sum(cliente[2]))#pegando e spmado as notas do cliente
+
+print(len(cliente[2]))#contando a quantidade de elemetos dentro da sub-lista
+
+print(sum(cliente[2])/len(cliente[2]))#calculando a media das notas (o comado 'sum'esta pegando a sub-lista[2] e
+#esta somado os elemetos e 'len' esta pegando a qunatidade de elemetos)
+
