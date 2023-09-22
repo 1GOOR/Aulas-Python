13bc7783979253130c370b122bb20306076f3ecf
igor.jsmorais <igorjosuesilva13@gmail.com>
2023-09-22 12:00:41 -0300

    dia 22/09

--- AppSeguranca/telaLogin.py
+++ AppSeguranca/telaLogin.py
@@ -32,10 +32,86 @@ def recuperarSenha():
 
 def telaLogin():
 
+    cargos = ["Gerente", "TI", "Administração", "Recursos Humanos"]
+
     layout = [
-        [sg.Image("img/logo.png", background_color="#2F6073"), sg.Text("Ola", font="Helvetica 8", text_color="#ffffff", background_color="#2F6073"), sg.Push(background_color="#2F6073")],
-        [sg.Text("Nome", background_color="#2F6073"), sg.Input(size=20, background_color="#ffffff", font=" roboto 15")]
+        [sg.Image("img/logo.png", background_color="#2F6073"),
+         sg.Push(background_color="#2F6073"),
+         sg.Text("CADASTRO DE FUNCIORIO", font=("Helvetica", 18), text_color="#ffffff", background_color="#2F6073"),
+         sg.Push(background_color="#2F6073")],
+
+        [sg.Push(background_color="#2F6073")],
+        [sg.Push(background_color="#2F6073")],
+        [sg.HSep()],
+        [sg.Push(background_color="#2F6073")],
+        [sg.Push(background_color="#2F6073")],
+
+        [sg.Text("Nome", size=5, background_color="#2F6073", font=("Helvetica", 14)),
+         sg.Input(size=(50, 30), background_color="#ffffff", font=("Helvetica", 14)),
+         sg.Text("Nascimeto", background_color="#2F6073", font=("Helvetica", 14)),
+         sg.Input(size=(15, 15), background_color="#ffffff", font=("Helvetica", 14))],
+
+        [sg.Text("CPF", size=5, text_color="#FFFFFF", font=("Helvetica", 14), background_color="#2F6073"),
+         sg.Input(size=30, background_color="#FFFFFF", font=("Helvetica", 14)),
+         sg.Text("Cargo", size=5, text_color="#FFFFFF", font=("Helvetica", 14), background_color="#2F6073"),
+         sg.Combo(cargos, size=16, default_value="Escolha um cargo", font=("Helvetica", 14)),
+         sg.Text("Cadastrar contato", text_color="#FFFFFF", font=("Helvetica", 14), background_color="#2F6073"),
+         sg.Image("img/contato.png", background_color="#2F6073")],
+
+        [sg.Push(background_color="#2F6073")],
+        [sg.Push(background_color="#2F6073")],
+        [sg.HSep()],
+        [sg.Push(background_color="#2F6073")],
+        [sg.Push(background_color="#2F6073")],
+
+        [sg.Text("Senha", size=5, background_color="#2F6073", text_color="#FFFFFF", font=("Helvetica", 14)),
+         sg.Input(size=(30, 30), background_color="#ffffff", font=("Helvetica", 14), password_char=" "),
+         sg.Text("Nivel", size=5, background_color="#2F6073", text_color="#FFFFFF", font=("Helvetica", 14)),
+         sg.Radio("ADM", "Ratio1", text_color="#FFFFFF", font=("Helvetica", 14), background_color="#2F6073"),
+         sg.Radio("Comum", "Ratio1", default=True, text_color="#FFFFFF", font=("Helvetica", 14), background_color="#2F6073"),
+         sg.Push(background_color="#2F6073"),
+         sg.Button("Cadastrar", size=20),
+         sg.Push(background_color="#2F6073"),
+         sg.Push(background_color="#2F6073"),
+         sg.Push(background_color="#2F6073"),
+         ]
     ]
     return sg.Window("Recuperar Senha", layout, background_color="#2F6073")
 
-telaLogin().read()
+def contatos():
+
+    estados = ["SP", "RJ", "BA", "BH", "PA", "RS"]
+
+    layout = [
+        [sg.Image("img/logo.png", background_color="#2F6073"),
+         sg.Push(background_color="#2F6073"),
+         sg.Text("CONTATOS", font=("Helvetica", 18), text_color="#ffffff", background_color="#2F6073"),
+         sg.Push(background_color="#2F6073")],
+
+        [sg.HSep()],
+
+        [sg.Text("Email", size=6, background_color="#2F6073", text_color="#FFFFFF", font=("Helvetica", 14)),
+         sg.Input(size=35, background_color="#ffffff", font=("Helvetica", 14)),
+         sg.Text("Telefone", size=8, background_color="#2F6073", text_color="#FFFFFF", font=("Helvetica", 14)),
+         sg.Input(size=22, background_color="#ffffff", font=("Helvetica", 14))],
+
+        [sg.Text("Rua", size=6, background_color="#2F6073", text_color="#FFFFFF", font=("Helvetica", 14)),
+         sg.Input(size=27, background_color="#ffffff", font=("Helvetica", 14)),
+         sg.Text("Nº", size=2, background_color="#2F6073", text_color="#FFFFFF", font=("Helvetica", 14)),
+         sg.Push(background_color="#2F6073"),
+         sg.Input(size=3, background_color="#ffffff", font=("Helvetica", 14)),
+         sg.Text("CEP", size=8, background_color="#2F6073", text_color="#FFFFFF", font=("Helvetica", 14)),
+         sg.Input(size=22, background_color="#ffffff", font=("Helvetica", 14))],
+
+        [sg.Text("Estado", size=6, background_color="#2F6073", text_color="#FFFFFF", font=("Helvetica", 14)),
+         sg.Combo(estados, size=3, default_value="SP", font=("Helvetica", 14)),
+         sg.Text("Cidade", size=6, background_color="#2F6073", text_color="#FFFFFF", font=("Helvetica", 14)),
+         sg.Push(background_color="#2F6073"),
+         sg.Input(size=22, background_color="#ffffff", font=("Helvetica", 14)),
+         sg.Text("Bairro", size=8, background_color="#2F6073", text_color="#FFFFFF", font=("Helvetica", 14)),
+         sg.Input(size=22, background_color="#ffffff", font=("Helvetica", 14))]
+
+    ]
+    return sg.Window("Contatos", layout, background_color="#2F6073")
+
+contatos().read()
