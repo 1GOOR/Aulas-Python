0268db01dab275730f83e0bc22d18800631f8c5e
igor.jsmorais <igorjosuesilva13@gmail.com>
2023-10-06 11:53:13 -0300

    dia 06/10

--- poo/teste.py
+++ poo/teste.py
@@ -1,1 +1,17 @@
-print("apenas um teste")
+import poo.associacao as so
+
+piloto_1 = so.Piloto("ze")
+piloto_2 = so.Piloto("Pedro")
+piloto_3 = so.Piloto("ana")
+piloto_4 = so.Piloto("gui")
+
+carro_1 = so.Carro("S1000")
+carro_2 = so.Carro("BMW M3")
+
+piloto_1.Carro = carro_1
+
+print(piloto_1.Carro.nome)
+
+piloto_3.Carro = carro_2
+
+piloto_3.Carro.dirigir()
